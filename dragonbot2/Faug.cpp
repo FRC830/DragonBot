6dd73d958c4d869c63de0fff29d0537626b61e9f
werehyena <morton.scott.e@gmail.com>
2014-08-17 15:48:05 -0400

    added windriver project

---
+++ dragonbot2/Faug.cpp
@@ -0,0 +1,39 @@
+#include "WPILib.h"
+
+class Faug : public IterativeRobot {
+public:
+	Faug() {
+		
+	}
+	
+	void RobotInit(){
+		
+	}
+	
+	void DisabledInit(){
+		
+	}
+	
+	void DisabledPeriodic(){
+		
+	}
+	
+	void TeleopInit(){
+		
+	}
+	
+	void TeleopPeriodic(){
+		
+	}
+	
+	void TestInit(){
+		
+	}
+	
+	void TestPeriodic(){
+		
+	}
+};
+
+START_ROBOT_CLASS(Faug);
+
